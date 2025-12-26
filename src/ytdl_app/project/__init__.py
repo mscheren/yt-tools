4345fdebf0db335c8d6d9f9b86c116f04c6c946f
mscheren <marcel.scherenberg@t-online.de>
2025-12-26 23:28:11 +0100

    fix(project): export OperationType in __init__

--- src/ytdl_app/project/__init__.py
+++ src/ytdl_app/project/__init__.py
@@ -1,7 +1,7 @@
 """Project management functionality."""
 
 from .batch import BatchJob, BatchProcessor
-from .config import EditOperation, ProjectConfig
+from .config import EditOperation, OperationType, ProjectConfig
 from .history import EditHistory
 from .manager import ProjectManager
 
@@ -10,6 +10,7 @@ __all__ = [
     "BatchProcessor",
     "EditHistory",
     "EditOperation",
+    "OperationType",
     "ProjectConfig",
     "ProjectManager",
 ]
